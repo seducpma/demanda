499bc2411a2cbc1579a3d5d8421cb58c2c51a09a
Desenvolvimento <you@example.com>
2020-08-26 13:37:01 -0300

    26-08-2020

--- app/controllers/criancas_controller.rb
+++ app/controllers/criancas_controller.rb
@@ -375,7 +375,7 @@ end
                     else
                         data=@crianca.nascimento.strftime("%Y-%m-%d")
                     end
-                     @crianca.recadastrada = 1
+                    @crianca.recadastrada = 1
                     hoje = Date.today.to_s
                     final = '2012-07-01'
                     if (hoje > data)  and (data >= final)
@@ -414,7 +414,7 @@ end
                         w1=@crianca.local_trabalho
 
                           if session[:show]==1
-                                                session[:show]=2
+                             session[:show]=2
                             format.html { redirect_to(@crianca) }
                             @crianca.recadastrada=session[:novo_cadastrar]
                             @crianca.save
@@ -434,8 +434,9 @@ end
                                 end
                             @crianca.save
 
+
                             if session[:ficha_pre]==1
-                                  session[:show]=3
+                                session[:show]=3
                                 mes=@crianca.nascimento.strftime("%m")
                                 ano=@crianca.nascimento.strftime("%Y")
                                 teste = ano+'-'+mes   ### veja abaixo VVVVV
@@ -460,6 +461,39 @@ end
                                  format.xml  { head :ok }
                                  #session[:show]=0
                             end
+                         else
+                            if session[:ficha_pre]==1
+                                  session[:show]=11
+                                mes=@crianca.nascimento.strftime("%m")
+                                ano=@crianca.nascimento.strftime("%Y")
+                                teste = ano+'-'+mes   ### veja abaixo VVVVV
+                               if  teste == '2017-04' or  teste == '2017-05' or teste == '2017-06' or teste == '2016-04' or  teste == '2016-05' or teste == '2016-06' or teste == '2015-04' or  teste == '2015-05' or teste == '2015-06'
+                                  @crianca.regiao_id=999
+                               end
+
+                                if @crianca.opcao2== '1'
+                                     @crianca.opcao2='estudou em outra unidade'
+                                end
+                                if @crianca.declaracao==true or @crianca.trabalho==true
+                                  @crianca.opcao1='trabalha'
+                                else
+                                  @crianca.opcao1='não trabalha'
+                                end
+                                @crianca.save
+                                 format.html { redirect_to(show_pre_path) }
+                                 format.xml  { head :ok }
+
+                            else
+                                 format.xml  { render :xml => @crianca, :status => :created, :location => @crianca }
+                                 format.xml  { head :ok }
+                                 #session[:show]=0
+                            end
+
+
+
+
+
+
                          end
                             if session[:ficha_pre]==1
                                   session[:show]=10
