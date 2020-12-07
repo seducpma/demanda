842ada0c8be49db83476d530a57bd117aed838f8
Desenvolvimento <you@example.com>
2020-12-07 10:16:16 -0200

    07-12-2020

--- app/controllers/criancas_controller.rb
+++ app/controllers/criancas_controller.rb
@@ -403,7 +403,7 @@ end
 
     #inscrição permitida para crianças após outubro/2020
 
-              if @crianca.nascimento.strftime("%Y%m%d").to_i > 20200803
+              if @crianca.nascimento.strftime("%Y%m%d").to_i > 20200807
                    respond_to do |format|
                         flash[:notice] = 'INSCRIÇÃO NÃO PERMITIDA.'
                         format.html { render :action => "aviso" }
