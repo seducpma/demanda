9f1d5a78a4a4502ba617477f95a25acadd86b730
Desenvolvimento <you@example.com>
2021-10-01 08:01:49 -0300

    01-10-2021 1530

--- config/routes.rb
+++ config/routes.rb
@@ -84,8 +84,8 @@ ActionController::Routing::Routes.draw do |map|
   map.pre_criancas '/pre_criancas', :controller => 'criancas', :action => 'pre_criancas'
   map.criancas_pre '/criancas_pre', :controller => 'criancas', :action => 'criancas_pre'
         #comentar  VVVV
-  #map.new_pre '/preescola1', :controller => 'criancas', :action => 'new_pre'
-  map.new_pre '/preescola1', :controller => 'criancas', :action => 'fora_ar'
+  map.new_pre '/preescola1', :controller => 'criancas', :action => 'new_pre'
+  #map.new_pre '/preescola1', :controller => 'criancas', :action => 'fora_ar'
       #descomentar VVVV
   #map.new_pre '/preescola', :controller => 'criancas', :action => 'new_pre'
        #comentar  VVVV
